4baf3d9e8e50198a8299e3bedb1b9ae2f6f8f320
unknown <samantha.c.egge@gmail.com>
2019-07-24 12:42:34 -0700

    Mocked out redux actions for app, added connect and state to App.js.

---
+++ src/store/actions/index.js
@@ -0,0 +1,12 @@
+export const ADD_FEATURE = 'ADD_FEATURE'
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+
+export const addCareFeature = (featureID) => {
+  console.log(featureID);
+  return { type: ADD_FEATURE, payload: featureID }
+}
+
+export const removeCarFeature = (featureID) => {
+  console.log(featureID);
+  return { type: REMOVE_FEATURE, payload: featureID}
+}
